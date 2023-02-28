fb084e601c0379db2524a4cc95000cf7b74837cb
Lasse Lang <lang.lasse@gmx.de>
2023-02-28 21:34:59 +0100

    Some code changes

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ class Main implements ActionListener {
 
   private JFrame frame;
   private JPanel panel;
-  private JButton button11, button12, button13, button21, button22, button23, button31, button32,  button33;
+  private JButton[] buttons = new JButton[9];
   private JLabel winLabel;
   private JButton newGame;
 
@@ -26,37 +26,15 @@ class Main implements ActionListener {
     panel.setBorder(BorderFactory.createEmptyBorder(20, 30, 30, 30));
     panel.setLayout(new GridLayout(3, 3));
 
-    button11 = new JButton();
-    button12 = new JButton();
-    button13 = new JButton();
-    button21 = new JButton();
-    button22 = new JButton();
-    button23 = new JButton();
-    button31 = new JButton();
-    button32 = new JButton();
-    button33 = new JButton();
+    for (int i = 0; i < buttons.length; i++) {
+      buttons[i] = new JButton();
+      buttons[i].addActionListener(this);
+      panel.add(buttons[i]);
+    }
+
     newGame = new JButton("New Game");
 
-    button11.addActionListener(this);
-    button12.addActionListener(this);
-    button13.addActionListener(this);
-    button21.addActionListener(this);
-    button22.addActionListener(this);
-    button23.addActionListener(this);
-    button31.addActionListener(this);
-    button32.addActionListener(this);
-    button33.addActionListener(this);
     newGame.addActionListener(this);
-
-    panel.add(button11);
-    panel.add(button12);
-    panel.add(button13);
-    panel.add(button21);
-    panel.add(button22);
-    panel.add(button23);
-    panel.add(button31);
-    panel.add(button32);
-    panel.add(button33);
 
     frame.add(panel, BorderLayout.CENTER);
     frame.add(winLabel, BorderLayout.NORTH);
@@ -70,15 +48,9 @@ class Main implements ActionListener {
 
   public void actionPerformed(ActionEvent e) {
     CheckNewGame(e);
-    CheckButton(e, button11, 0);
-    CheckButton(e, button12, 1);
-    CheckButton(e, button13, 2);
-    CheckButton(e, button21, 3);
-    CheckButton(e, button22, 4);
-    CheckButton(e, button23, 5);
-    CheckButton(e, button31, 6);
-    CheckButton(e, button32, 7);
-    CheckButton(e, button33, 8);
+    for (int i = 0; i < buttons.length; i++) {
+      CheckButton(e, buttons[i], i);
+    }
     CheckWin();
     SetWinner();
   }
@@ -92,10 +64,10 @@ class Main implements ActionListener {
   }
 
   private void ChangeTurn() {
-    if (userTurn.equals("X")) {
+    if (userTurn == "X") {
       userTurn = "O";
     }
-    else if (userTurn.equals("O")) {
+    else if (userTurn == "O") {
       userTurn = "X";
     }
   }
@@ -114,14 +86,14 @@ class Main implements ActionListener {
 
   private Boolean WinCheck(String player) {
     return (
-      (fields[0].equals(player) && fields[1].equals(player) && fields[2].equals(player)) ||
-      (fields[3].equals(player) && fields[4].equals(player) && fields[5].equals(player)) ||
-      (fields[6].equals(player) && fields[7].equals(player) && fields[8].equals(player)) || 
-      (fields[0].equals(player) && fields[3].equals(player) && fields[6].equals(player)) ||
-      (fields[1].equals(player) && fields[4].equals(player) && fields[7].equals(player)) ||
-      (fields[2].equals(player) && fields[5].equals(player) && fields[8].equals(player)) ||
-      (fields[0].equals(player) && fields[4].equals(player) && fields[8].equals(player)) ||
-      (fields[2].equals(player) && fields[4].equals(player) && fields[6].equals(player)));
+      (fields[0] == player && fields[1] == player && fields[2] == player) ||
+      (fields[3] == player && fields[4] == player && fields[5] == player) ||
+      (fields[6] == player && fields[7] == player && fields[8] == player) || 
+      (fields[0] == player && fields[3] == player && fields[6] == player) ||
+      (fields[1] == player && fields[4] == player && fields[7] == player) ||
+      (fields[2] == player && fields[5] == player && fields[8] == player) ||
+      (fields[0] == player && fields[4] == player && fields[8] == player) ||
+      (fields[2] == player && fields[4] == player && fields[6] == player));
   }
 
   private void SetWinner() {
@@ -138,15 +110,9 @@ class Main implements ActionListener {
 
   private void CheckNewGame(ActionEvent e) {
     if (e.getSource() == newGame) {
-      button11.setText("");
-      button12.setText("");
-      button13.setText("");
-      button21.setText("");
-      button22.setText("");
-      button23.setText("");
-      button31.setText("");
-      button32.setText("");
-      button33.setText("");
+      for (int i = 0; i < buttons.length; i++) {
+        buttons[i].setText("");
+      }
       for (int i = 0; i < 9; i++) {
         fields[i] = "";
       }
